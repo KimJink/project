0b50d814b26cabf6270ef76b2947d4c0c3371da2
Kwang <wlsrhkd0101@gmail.com>
2021-12-24 15:59:21 +0900

    commit

---
+++ src/main/java/com/project/web/dto/PostsResponseDto.java
@@ -0,0 +1,19 @@
+package com.project.web.dto;
+
+import com.project.web.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
